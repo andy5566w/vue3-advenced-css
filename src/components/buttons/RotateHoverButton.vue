bdff58e3f1b686e6b4a5334290b308880066b82c
andy.tsai <andy.tsai@deephow.com>
2022-10-21 00:16:11 +0800

    init + rotate button

---
+++ src/components/buttons/RotateHoverButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex items-center justify-center">
+    <a href="#" class="rotate text-lg p-14">hover me</a>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.rotate {
+  position: relative;
+  overflow: hidden;
+  &:before {
+    content: '';
+    width: 100%;
+    background-color: red;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    transform-origin: bottom left;
+    transform: rotate(-90deg);
+    transition: transform 0.5s ease-in;
+    border: 1px solid black;
+  }
+
+  &:hover:before {
+    transform: rotate(0deg);
+  }
+}
+</style>
