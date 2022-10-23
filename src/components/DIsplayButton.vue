9172552f0203f87b4847ba7432526c0360486eaa
andy.tsai <andy.tsai@deephow.com>
2022-10-23 14:04:52 +0800

    swipe button

--- src/components/DIsplayButton.vue
+++ src/components/DIsplayButton.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="flex items-center justift-center">
+  <div class="flex items-center justift-center gap-1">
     <RotateHoverButton />
+    <SwipeButton />
   </div>
 </template>
 
 <script setup>
 import RotateHoverButton from './buttons/RotateHoverButtons.vue'
+import SwipeButton from './buttons/SwipeButton.vue'
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/components/buttons/SwipeButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <a class="swipe-button px-8 py-4 border-4 border-current relative" href="#"
+    >Hover me</a
+  >
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.swipe-button {
+  overflow: hidden;
+  &:before {
+    transition: all 0.5s ease-in;
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: currentColor;
+    transform: translateX(-100%);
+    z-index: -1;
+  }
+  &:hover:before {
+    transform: translateX(0);
+    color: white;
+  }
+}
+</style>
