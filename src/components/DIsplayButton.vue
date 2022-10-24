7058e07972f640d28c98f493d6650622312dfb97
andy.tsai <andy.tsai@deephow.com>
2022-10-25 00:17:36 +0800

    add swipe button with text

--- src/components/DIsplayButton.vue
+++ src/components/DIsplayButton.vue
@@ -2,6 +2,7 @@
   <div class="flex items-center justift-center gap-4">
     <RotateHoverButton />
     <SwipeButton />
+    <SwipeButtonWithText />
     <SpreadHoverButton />
     <StrenthButton />
   </div>
@@ -12,6 +13,7 @@ import RotateHoverButton from './buttons/RotateHoverButtons.vue'
 import SwipeButton from './buttons/SwipeButton.vue'
 import SpreadHoverButton from './buttons/SpreadHoverButton.vue'
 import StrenthButton from './buttons/StrengthButton.vue'
+import SwipeButtonWithText from './buttons/SwipeButtonWithText.vue'
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/components/buttons/SwipeButtonWithText.vue
@@ -0,0 +1,35 @@
+<template>
+  <a
+    class="swipe-button px-8 py-4 border-4 border-current relative"
+    href="#"
+    data-text="Yep!"
+    >Swipe Button With Text</a
+  >
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.swipe-button {
+  overflow: hidden;
+  &:before {
+    display: block;
+    transition: all 0.5s ease-in;
+    content: attr(data-text);
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: currentColor;
+    transform: translateX(-100%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  &:hover:before {
+    transform: translateX(0);
+    background: #fff;
+  }
+}
+</style>
