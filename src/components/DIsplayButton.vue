d236364518bd66f9aa8354dc883d963bb243ae69
andy.tsai <andy.tsai@deephow.com>
2022-10-24 23:57:48 +0800

    add strength button

--- src/components/DIsplayButton.vue
+++ src/components/DIsplayButton.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="flex items-center justift-center gap-1">
+  <div class="flex items-center justift-center gap-4">
     <RotateHoverButton />
     <SwipeButton />
     <SpreadHoverButton />
+    <StrenthButton />
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import RotateHoverButton from './buttons/RotateHoverButtons.vue'
 import SwipeButton from './buttons/SwipeButton.vue'
 import SpreadHoverButton from './buttons/SpreadHoverButton.vue'
+import StrenthButton from './buttons/StrengthButton.vue'
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/components/buttons/StrengthButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <a
+    class="px-8 py-4 border-4 hover:border-8 font-medium hover:font-black border-current relative"
+    href="#"
+    >Strength button</a
+  >
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+a {
+  transition: 0.5s;
+}
+</style>
