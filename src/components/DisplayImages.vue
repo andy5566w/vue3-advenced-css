93f42b8690e6155e28c7856981c6cb6f7e966e76
andy.tsai <andy.tsai@deephow.com>
2022-10-30 11:22:16 +0800

    add Moving image

--- src/components/DisplayImages.vue
+++ src/components/DisplayImages.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
+  <div class="flex flex-col gap-8">
     <FadingImage />
+    <MovingImage />
   </div>
 </template>
 
 <script setup>
 import FadingImage from '../components/images/FadingImage.vue'
+import MovingImage from '../components/images/MovingImage.vue'
 </script>
 
 <style scoped></style>
---
+++ src/components/images/MovingImage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="imageContainer">
+    <img src="/src/assets/images/bali.jpeg" alt="tower" />
+    <div class="caption">
+      <h2 class="uppercase font-bold text-3xl">caption!</h2>
+      <p class="text-xl">You can write some review here</p>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped lang="scss">
+.imageContainer {
+  position: relative;
+  height: 500px;
+  overflow: hidden;
+  img {
+    height: 100%;
+    transition: all 0.5s;
+  }
+
+  .caption {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 30%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: rgba(0, 0, 0, 0.5);
+    transition: all 0.5s;
+    transform: translateY(100%);
+  }
+
+  &:hover {
+    .caption {
+      opacity: 1;
+      transform: translateY(0);
+    }
+
+    img {
+      transform: translateY(-30%);
+    }
+  }
+}
+</style>
