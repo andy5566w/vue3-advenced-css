546bc60ef845e0bf92d3a924e96b38a7e42968c3
andy.tsai <andy.tsai@deephow.com>
2022-10-30 12:23:43 +0800

    add DelayImage.vue

--- src/components/DisplayImages.vue
+++ src/components/DisplayImages.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="flex flex-col gap-8">
+  <div class="flex flex-col gap-8 items-center">
     <FadingImage />
     <MovingImage />
+    <DelayImage />
   </div>
 </template>
 
 <script setup>
 import FadingImage from '../components/images/FadingImage.vue'
 import MovingImage from '../components/images/MovingImage.vue'
+import DelayImage from './images/DelayImage.vue'
 </script>
 
 <style scoped></style>
---
+++ src/components/images/DelayImage.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="delay-img">
+    <BaseImage
+      image-path="/src/assets/images/beach.jpeg"
+      caption="Caption!!"
+      content="Please type something..."
+    />
+  </div>
+</template>
+
+<script setup>
+import BaseImage from './BaseImage.vue'
+</script>
+
+<style lang="scss">
+.delay-img {
+  position: relative;
+  overflow: hidden;
+  &:before {
+    content: '';
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: #006266;
+    opacity: 0.5;
+    transition: all 0.25s;
+    transform: translateY(100%);
+    transition-delay: 0.25s;
+    z-index: 1;
+  }
+
+  .caption {
+    transition: all 0.25s;
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: transparent;
+    transform: translateY(-20px);
+    z-index: 2;
+    p {
+      background-color: #ae895d;
+      margin: 0 auto;
+      border-radius: 5px;
+      padding: 3px 10px;
+    }
+  }
+
+  &:hover {
+    &:before {
+      transform: translateY(0);
+      transition-delay: 0s;
+    }
+
+    .caption {
+      opacity: 1;
+      transform: translateY(0);
+      transition-delay: 0.25s;
+    }
+  }
+}
+</style>
