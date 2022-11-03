51256660a4769657db2e47d6ec11b624e4e4bc4c
andy.tsai <andy.tsai@deephow.com>
2022-11-03 16:10:14 +0800

    add DisplayBlock.vue and set router

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,7 @@
-<script setup>
-const lists = [
-  {
-    name: { name: 'displayButton' },
-    text: 'display button',
-  },
-  {
-    name: { name: 'displayImg' },
-    text: 'display image',
-  },
-  {
-    name: { name: 'displayManu' },
-    text: 'display menus',
-  },
-  {
-    name: { name: 'displaySpecialAnimation' },
-    text: 'display special animation',
-  },
-]
-</script>
+<script setup></script>
 
 <template>
-  <div class="container">
-    <ul class="flex items-center justify-center gap-4 py-4">
-      <li class="capitalize" v-for="{ name, text } in lists" :key="text">
-        <router-link tag="a" :to="name">{{ text }}</router-link>
-      </li>
-    </ul>
-    <router-view />
-  </div>
+  <router-view />
 </template>
 
-<style scoped lang="scss">
-li {
-  &:not(:last-child) {
-    padding-right: 1rem;
-    border-right: 2px solid #646cff;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,23 +2,40 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    name: 'displayButton',
-    component: () => import('../components/DisplayButton.vue'),
+    redirect: () => ({
+      name: 'displayButton',
+    }),
   },
   {
-    path: '/img',
-    name: 'displayImg',
-    component: () => import('../components/DisplayImages.vue'),
+    path: '/display',
+    component: () => import('../pages/DisplayBlock.vue'),
+    children: [
+      {
+        path: '',
+        name: 'displayButton',
+        component: () => import('../components/DisplayButton.vue'),
+      },
+      {
+        path: 'img',
+        name: 'displayImg',
+        component: () => import('../components/DisplayImages.vue'),
+      },
+      {
+        path: 'menus',
+        name: 'displayManu',
+        component: () => import('../components/DisplayMenus.vue'),
+      },
+      {
+        path: 'specialAnimation',
+        name: 'displaySpecialAnimation',
+        component: () => import('../components/DisplaySpecialAnimation.vue'),
+      },
+    ],
   },
   {
-    path: '/menus',
-    name: 'displayManu',
-    component: () => import('../components/DisplayMenus.vue'),
-  },
-  {
-    path: '/specialAnimation',
-    name: 'displaySpecialAnimation',
-    component: () => import('../components/DisplaySpecialAnimation.vue'),
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import('../pages/NotFound.vue'),
   },
 ]
 
