4474453b4e7b02d6ca19fe0852362a0f85ea61ee
andy.tsai <andy.tsai@deephow.com>
2022-11-03 14:46:52 +0800

    add FillText.vue

--- src/components/DisplaySpecialAnimation.vue
+++ src/components/DisplaySpecialAnimation.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="flex flex-col items-center">
     <SmokeText />
+    <FillText />
   </div>
 </template>
 
 <script setup>
 import SmokeText from './animations/SmokeText.vue'
+import FillText from './animations/FillText.vue'
 </script>
 
 <style scoped></style>
---
+++ src/components/animations/FillText.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1 data-text="TEXT">Text</h1>
+</template>
+
+<script></script>
+
+<style scoped lang="scss">
+h1 {
+  font-weight: bold;
+  font-size: 5rem;
+  text-transform: uppercase;
+  position: relative;
+  color: #fff;
+
+  &:before {
+    content: attr(data-text);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0;
+    height: 100%;
+    color: #535bf2;
+    overflow: hidden;
+    transition: 0.5s;
+  }
+
+  &:hover {
+    &:before {
+      width: 100%;
+    }
+  }
+}
+</style>
