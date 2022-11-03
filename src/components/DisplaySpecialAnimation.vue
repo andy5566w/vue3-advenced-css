0e58693d648f4eab180a033e4a4ba167bca26077
andy.tsai <andy.tsai@deephow.com>
2022-11-03 15:22:18 +0800

    add hero icon and SocialMediaIcon.vue

--- src/components/DisplaySpecialAnimation.vue
+++ src/components/DisplaySpecialAnimation.vue
@@ -2,12 +2,14 @@
   <div class="flex flex-col items-center">
     <SmokeText />
     <FillText />
+    <SocialMediaIcon />
   </div>
 </template>
 
 <script setup>
 import SmokeText from './animations/SmokeText.vue'
 import FillText from './animations/FillText.vue'
+import SocialMediaIcon from './animations/SocialMediaIcon.vue'
 </script>
 
 <style scoped></style>
---
+++ src/components/animations/SocialMediaIcon.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container my-8">
+    <ul class="flex items-center gap-8 justify-center">
+      <li>
+        <a href="#"><AcademicCapIcon class="w-10 icon" /></a>
+      </li>
+      <li>
+        <a href="#"><CakeIcon class="w-10 icon" /></a>
+      </li>
+      <li>
+        <a href="#"><BugAntIcon class="w-10 icon" /></a>
+      </li>
+      <li>
+        <a href="#"><CreditCardIcon class="w-10 icon" /></a>
+      </li>
+      <li>
+        <a href="#"><EnvelopeIcon class="w-10 icon" /></a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import {
+  AcademicCapIcon,
+  CakeIcon,
+  BugAntIcon,
+  CreditCardIcon,
+  EnvelopeIcon,
+} from '@heroicons/vue/24/outline'
+</script>
+
+<style lang="scss" scoped>
+a {
+  display: inline-block;
+  border-radius: 15px;
+  background-color: #fff;
+  padding: 1rem;
+  box-shadow: 10px 5px 10px black;
+  position: relative;
+  transition: 0.5s;
+  overflow: hidden;
+
+  .icon {
+    position: relative;
+    z-index: 2;
+  }
+  &:before {
+    content: '';
+    position: absolute;
+    left: -100%;
+    top: 130%;
+    width: 150%;
+    height: 150%;
+    background-color: #535bf2;
+    transform: rotate(45deg);
+  }
+
+  &:hover {
+    .icon {
+      color: #fff;
+    }
+    &:before {
+      animation: moving 0.5s 1 forwards;
+    }
+  }
+}
+
+@keyframes moving {
+  0% {
+    left: -100%;
+    top: 130%;
+  }
+
+  50% {
+    left: 5%;
+    top: -40%;
+  }
+
+  100% {
+    left: -20%;
+    top: -20%;
+  }
+}
+</style>
