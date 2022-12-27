d6706c03332c85ab25ee4aee6d85d01bf25d62e6
andy.tsai <andy.tsai@deephow.com>
2022-12-27 18:03:31 +0800

    add rotateLoadingRect.vue

--- src/components/DisplayAnimationKey.vue
+++ src/components/DisplayAnimationKey.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="flex flex-col gap-8 items-center">
     <FloatingText />
+    <RotateLoadingRect />
   </div>
 </template>
 
 <script setup>
 import FloatingText from './animations/FloatingText.vue'
+import RotateLoadingRect from './animations/rotateLoadingRect.vue'
 </script>
 
 <style scoped></style>
---
+++ src/components/animations/rotateLoadingRect.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <div class="loading"></div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss">
+section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  perspective: 200px;
+  background-color: #26de81;
+  width: 100%;
+  height: 150px;
+  .loading {
+    animation: loading 2s linear alternate infinite;
+    border-radius: 12px;
+    background-color: #fff;
+    width: 100px;
+    height: 100px;
+  }
+}
+
+@keyframes loading {
+  0% {
+    transform: rotateX(0deg) rotateY(0deg);
+  }
+
+  50% {
+    transform: rotateX(0deg) rotateY(180deg);
+  }
+
+  100% {
+    transform: rotateX(180deg) rotateY(180deg);
+  }
+}
+</style>
