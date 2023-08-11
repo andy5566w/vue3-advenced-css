391499913e382525799f2fae6e3b2534d08d252f
andy.tsai <andy.tsai@deephow.com>
2023-08-11 11:45:07 +0800

    t

--- src/pages/audio/Audio.vue
+++ src/pages/audio/Audio.vue
@@ -5,8 +5,7 @@
   </div>
   <div class="button">
     <button @click="handleStartWhiteNoise">white noise</button>
-    <button @click="handleBiQuad" class="mx-2">pink noise</button>
-    <button @click="clearBuffer" class="mx-2">stop</button>
+    <button @click="handlePinkNoise" class="mx-2">pink noise</button>
   </div>
   <audio-note />
 </template>
@@ -19,14 +18,14 @@ const whiteNoiseSource = ref(null)
 const primaryGainControl = ref(null)
 const volume = ref(0.05)
 
-const changeVolume = () => {
+const changeVolume = (volume) => {
   primaryGainControl.value = audioContentext.createGain()
   // gain: set audio volume，second argument is start time
-  primaryGainControl.value.gain.setValueAtTime(volume.value, 0)
+  primaryGainControl.value.gain.setValueAtTime(volume, 0)
   primaryGainControl.value.connect(audioContentext.destination)
 }
 
-const buildWhiteNoise = (seconds = 10) => {
+const buildWhiteNoiseBuffer = (seconds = 10) => {
   const buffer = audioContentext.createBuffer(
     1,
     audioContentext.sampleRate * seconds,
@@ -36,41 +35,38 @@ const buildWhiteNoise = (seconds = 10) => {
   for (let i = 0; i < buffer.length; i++) {
     channelData[i] = Math.random() * 2 - 1
   }
+  return buffer
+}
 
+const buildWhiteNoise = () => {
   const whiteNoiseSource = audioContentext.createBufferSource()
   whiteNoiseSource.connect(primaryGainControl.value)
-  whiteNoiseSource.buffer = buffer
+  whiteNoiseSource.buffer = buildWhiteNoiseBuffer()
   return whiteNoiseSource
 }
 
-const clearBuffer = (buffer) => {
-  if (!whiteNoiseSource.value) {
-    return
-  }
-  whiteNoiseSource.value.stop()
-  whiteNoiseSource.value = null
-}
-
 const handleStartWhiteNoise = () => {
-  clearBuffer()
   // Web API prevent memory leak and then you need to re-initialize new white noise every time.
-  whiteNoiseSource.value = buildWhiteNoise(5)
+  whiteNoiseSource.value = buildWhiteNoise(10)
   whiteNoiseSource.value.start()
+  whiteNoiseSource.value.stop(audioContentext.currentTime + 2)
 }
 
-const handleBiQuad = () => {
-  clearBuffer()
-  const snareFilter = audioContentext.createBiquadFilter()
-  snareFilter.type = 'highpass'
-  snareFilter.frequency.value = 500
-
-  whiteNoiseSource.value = buildWhiteNoise(2)
-  whiteNoiseSource.value.connect(snareFilter)
-  whiteNoiseSource.value.start()
+const handlePinkNoise = () => {
+  const pinkNoiseSource = audioContentext.createBufferSource()
+  pinkNoiseSource.buffer = buildWhiteNoiseBuffer()
+  primaryGainControl.value.gain.linearRampToValueAtTime(
+    0,
+    audioContentext.currentTime + 5
+  )
+  pinkNoiseSource.connect(primaryGainControl.value)
+  pinkNoiseSource.start()
+  pinkNoiseSource.stop(audioContentext.currentTime + 5)
+  changeVolume(Number(volume.value) * 0.01)
 }
 
 onMounted(() => {
-  changeVolume()
+  changeVolume(0.02)
 })
 
 watch(volume, (volume) => {
