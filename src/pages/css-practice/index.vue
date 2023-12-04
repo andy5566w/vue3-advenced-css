59658788718c581d403a8c08215721017529d2f2
andy5566w <38370595+andy5566w@users.noreply.github.com>
2023-12-04 23:30:50 +0800

    spinner practice

---
+++ src/pages/css-practice/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="h-screen">
+    <Spinner />
+  </div>
+</template>
+
+<script setup>
+import Spinner from '../../components/css-practice/Spinner.vue'
+</script>
+
+<style lang="scss" scoped></style>
