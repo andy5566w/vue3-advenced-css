52788fcec52d1e15290adfda2e222e67e15b030e
andy.tsai <andy.tsai@deephow.com>
2024-01-20 18:46:43 +0800

    add common func (randomColor)

--- src/js/zoomUtils/zommUtils.js
+++ src/js/zoomUtils/zommUtils.js
@@ -10,3 +10,10 @@ export const convertTimeIntoWidth = (time) => {
 export const convertWidthIntoTime = (width) => {
   return round((width / editor.currentUnit.width) * editor.currentUnit.time, 2)
 }
+
+export const randomColor = () => {
+  const r = Math.floor(Math.random() * 256)
+  const g = Math.floor(Math.random() * 256)
+  const b = Math.floor(Math.random() * 256)
+  return `rgb(${r},${g},${b})`
+}
