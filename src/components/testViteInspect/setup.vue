074d22d90fdeaa9ab32ff2b4e0352fbb599a4623
andy.tsai <andy.tsai@deephow.com>
2024-01-30 22:11:27 +0800

    practive setup and script setup

---
+++ src/components/testViteInspect/setup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="increase">increase</button>
+    {{ count }}
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup(_, { expose }) {
+    const count = ref(0)
+    expose({ count })
+
+    function increase() {
+      count.value++
+    }
+    return { count, increase }
+  },
+}
+</script>
+
+<style></style>
