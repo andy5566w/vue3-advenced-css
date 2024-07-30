29bbae6d8091956b0b60fdbd282fc3bf6600e1ed
andy.tsai <andy5566w@gmail.com>
2024-07-30 21:07:45 +0800

    add control container and button style

--- src/pages/youtube-player/YoutubePlayer.vue
+++ src/pages/youtube-player/YoutubePlayer.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="video-container">
+    <div class="video-controls-containers">
+      <div class="controls">
+        <button class="play-pause-btn">
+          <svg class="play-icon" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+          </svg>
+          <svg class="pause-icon" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M14,19H18V5H14M6,19H10V5H6V19Z" />
+          </svg>
+        </button>
+      </div>
+    </div>
     <video :src="videoUrl" controls></video>
   </div>
 </template>
@@ -11,6 +23,7 @@ import videoUrl from '@/assets/videos/cat-birthday.mp4'
 <style scoped lang="scss">
 .video-container {
   display: flex;
+  position: relative;
   justify-content: center;
   margin-inline: auto;
   width: 90%;
@@ -18,6 +31,57 @@ import videoUrl from '@/assets/videos/cat-birthday.mp4'
 
   video {
     width: 100%;
+  }
+
+  // &:focus-within -> if user press tab and focus any element in container. This style will be applied for.
+  &:hover,
+  &:focus-within,
+  &.pause {
+    .video-controls-containers {
+      opacity: 1;
+    }
+  }
+
+  &.pause .pause-icon {
+    display: none;
+  }
+
+  &:not(.pause) .play-icon {
+    display: none;
+  }
+
+  .video-controls-containers {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    opacity: 0;
+    z-index: 100;
+    transition: opacity 0.25s ease-in-out;
+    color: white;
+
+    .controls {
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+      padding: 0.25rem;
+
+      button {
+        opacity: 0.85;
+        transition: opacity 0.15s ease-in-out;
+        cursor: pointer;
+        border: none;
+        background: none;
+        padding: 0;
+        width: 30px;
+        height: 30px;
+        color: inherit;
+        font-size: 1.1rem;
+
+        &:hover {
+          opacity: 1;
+        }
+      }
+    }
   }
 }
 </style>
