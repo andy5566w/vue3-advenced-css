1e19aaeefe118330c633f60616ddcd71f5daba5e
andy.tsai <andy.tsai@deephow.com>
2024-08-06 00:51:50 +0800

    package component

--- src/components/popup/Message.vue
+++ src/components/popup/Message.vue
@@ -4,7 +4,7 @@
 
     <div class="dialog-container caret-gray-700">
       <p class="message">{{ message }}</p>
-      <button class="button" @click="$emit('close')">OK</button>
+      <button class="button" @click="$emit('close')">close</button>
     </div>
   </div>
 </template>
@@ -45,20 +45,28 @@ defineEmits(['close'])
     translate: -50% -50%;
     border: 1px solid #2d3436;
     border-radius: 5px;
-    background-color: cadetblue;
+    background-color: #242a2a;
     padding: 0.5rem;
     aspect-ratio: 2 / 1;
     width: 300px;
 
     .message {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
       font-size: 1.3rem;
     }
 
     .button {
       all: initial;
+      position: absolute;
+      right: 8px;
+      bottom: 8px;
       cursor: pointer;
       margin-left: auto;
       padding: 0.5rem;
+      color: #fff;
     }
   }
 }
--- src/pages/progress/index.vue
+++ src/pages/progress/index.vue
@@ -3,14 +3,14 @@ import DownloadProgress from '../../components/progress/DownloadProgress.vue'
 import showMessage from '../../js/packagePopup/showMessage.js'
 
 const handleClick = () => {
-  showMessage('message from index.vue', (close) => {
+  showMessage('This is warning', (close) => {
     close()
   })
 }
 </script>
 
 <template>
-  <DownloadProgress />
+  <!--  <DownloadProgress />-->
   <button @click="handleClick">show msg</button>
 </template>
 
