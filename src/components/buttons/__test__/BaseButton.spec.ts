e97b5aa45334ae4121db48dc8e9ed67efa6d3cdf
andy.tsai <andy.tsai@deephow.com>
2025-01-01 23:54:08 +0800

    add vitest

---
+++ src/components/buttons/__test__/BaseButton.spec.ts
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils'
+import { describe, it } from 'vitest'
+
+// npx vitest -> for execute
+describe('BaseButton', () => {
+  it('render component', () => {
+    console.log('test 123')
+  })
+})
